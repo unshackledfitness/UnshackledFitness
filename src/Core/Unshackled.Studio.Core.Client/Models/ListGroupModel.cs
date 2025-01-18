3e92f4d45c17d66104e6a8083faa0c4eb74ae0de
tonyrix <tony.rix@gmail.com>
2025-01-18 13:44:50 -0600

    Add store location description to change location drawers.

--- src/Core/Unshackled.Studio.Core.Client/Models/ListGroupModel.cs
+++ src/Core/Unshackled.Studio.Core.Client/Models/ListGroupModel.cs
@@ -4,5 +4,6 @@ public class ListGroupModel : ISortableGroup
 {
 	public string Sid { get; set; } = string.Empty;
 	public string Title { get; set; } = string.Empty;
+	public string? Description { get; set; }
 	public int SortOrder { get; set; }
 }
